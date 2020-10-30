c0bd46bb59a80438c3f0d82867bac1f4e4afcf59
layry <73444229+layry@users.noreply.github.com>
2020-10-30 21:08:19 +0300

    1
    1

---
+++ precache-manifest.ca2e4ffc8710f44190c295fae77b9a47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99cc0c10c360925dfce80db007baabc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2ec47b33b6904902ed3",
+    "url": "/static/css/2.6326594c.chunk.css"
+  },
+  {
+    "revision": "bf391cb8de2367b46d73",
+    "url": "/static/css/main.07ba2694.chunk.css"
+  },
+  {
+    "revision": "f2ec47b33b6904902ed3",
+    "url": "/static/js/2.73db65c3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.73db65c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf391cb8de2367b46d73",
+    "url": "/static/js/main.ea32325f.chunk.js"
+  },
+  {
+    "revision": "bde3be86d147a096631f",
+    "url": "/static/js/runtime-main.944b771c.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
