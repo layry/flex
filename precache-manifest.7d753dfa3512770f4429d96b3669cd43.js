126fba465fc4f50cf90b08c2ddbdf2fd86563545
layry <73444229+layry@users.noreply.github.com>
2020-10-30 21:57:51 +0300

    1
    1

---
+++ precache-manifest.7d753dfa3512770f4429d96b3669cd43.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a473fd0e447b9066f0c77f1c9c8d46aa",
+    "url": "/flex/index.html"
+  },
+  {
+    "revision": "7f7edf3e98279e2c2e9a",
+    "url": "/flex/static/css/2.6326594c.chunk.css"
+  },
+  {
+    "revision": "60a63aa6dcbc3f48c1ae",
+    "url": "/flex/static/css/main.07ba2694.chunk.css"
+  },
+  {
+    "revision": "7f7edf3e98279e2c2e9a",
+    "url": "/flex/static/js/2.ae860493.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/flex/static/js/2.ae860493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60a63aa6dcbc3f48c1ae",
+    "url": "/flex/static/js/main.0028052c.chunk.js"
+  },
+  {
+    "revision": "cd887e2bbd26b88d9fdf",
+    "url": "/flex/static/js/runtime-main.869500c5.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/flex/static/media/persik.4e1ec840.png"
+  }
+]);
